e3f7f0d87a52ec676a0b624f77b872bffd9719b8
Diego <sigma.octantis@gmai.com>
2018-01-02 00:34:30 -0300

    test script edition

--- pipeline.sh
+++ pipeline.sh
@@ -1,10 +1,8 @@
 cat test.txt | \
-curl -F 'input=<-' "http://192.168.99.100:9000" | \
-curl -F 'input=<-' "http://192.168.99.100:9001" | \
-curl -F 'input=<-' "http://192.168.99.100:9002" | \
-curl -F 'input=<-' "http://192.168.99.100:9003" | \
-curl -F 'input=<-' "http://192.168.99.100:9004" | \
-curl -F 'input=<-' "http://opener.olery.com/ned" | \
-curl -F 'input=<-' "http://opener.olery.com/property-tagger" | \
-curl -F 'input=<-' "http://opener.olery.com/polarity-tagger" | \
-curl -F 'input=<-' "http://opener.olery.com/opinion-detector"
+curl -F 'input=<-' "http://172.17.42.1:9000" | \
+curl -F 'input=<-' "http://172.17.42.1:9001" | \
+curl -F 'input=<-' "http://172.17.42.1:9002" | \
+curl -F 'input=<-' "http://172.17.42.1:9003" | \
+curl -F 'input=<-' "http://172.17.42.1:9004" | \
+curl -F 'input=<-' "http://172.17.42.1:9005" | \
+curl -F 'input=<-' "http://172.17.42.1:9006" 
